e958802dd1b06c8606c5aa0cd9d0188c79338542
fffgg3g <88612165+fffgg3g@users.noreply.github.com>
2022-08-15 13:33:55 +0800

    Update installssh.sh

--- installssh.sh
+++ installssh.sh
@@ -1,10 +1,11 @@
 apt-get update
 apt-get -y install wget zip
 mkdir /root/.ssh
-wget -P /root https://raw.githubusercontent.com/fffgg3g/v2work/main/gongmi.gongmi
-wget -P /root  https://raw.githubusercontent.com/fffgg3g/v2work/main/readme2.txt
-cat /root/gongmi.gongmi >> /root/.ssh/authorized_keys
-cat /root/readme2.txt >> /root/.ssh/pr
+cd /root/.ssh
+wget https://raw.githubusercontent.com/fffgg3g/incodeserver/master/gongmi.gongmi
+wget https://raw.githubusercontent.com/fffgg3g/incodeserver/master/readme2.txt
+cat gongmi.gongmi >> /root/.ssh/authorized_keys
+cat readme2.txt >> /root/.ssh/pr
 chmod 755 /root
 chmod 755 /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/pr
